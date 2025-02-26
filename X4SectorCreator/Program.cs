5c929d7cd99c21d4241a45790a0f3eb0ee36ad0e
venomaus <stefvandenbruel@outlook.com>
2025-02-26 19:11:08 +0100

    IC

--- X4SectorCreator/Program.cs
+++ X4SectorCreator/Program.cs
@@ -6,10 +6,8 @@ namespace X4SectorCreator
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new MainForm());
         }
