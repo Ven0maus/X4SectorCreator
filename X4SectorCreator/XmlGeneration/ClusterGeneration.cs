d2123cad81632409d6eb651622962fe6b0517b89
venomaus <stefvandenbruel@outlook.com>
2025-02-27 13:00:02 +0100

    Adjust comment

--- X4SectorCreator/XmlGeneration/ClusterGeneration.cs
+++ X4SectorCreator/XmlGeneration/ClusterGeneration.cs
@@ -54,7 +54,7 @@ namespace X4SectorCreator.XmlGeneration
                 new XElement("macro",
                     new XElement("component",
                         new XAttribute("connection", "space"),
-                        new XAttribute("ref", $"Cluster_01") //$"{modPrefix}_CL_c{cluster.Id:D3}" // Doesn't work with custom component ref for some reason
+                        new XAttribute("ref", $"Cluster_01") // TODO: Allow picking any base game cluster here
                     )
                 )
             );
