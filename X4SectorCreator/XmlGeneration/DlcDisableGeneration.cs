25f6fa18b09ea27c194b139e13786351f2ff1ff8
venomaus <stefvandenbruel@outlook.com>
2025-03-06 15:51:04 +0100

    Add support for disabling hyperion pack dlc content

--- X4SectorCreator/XmlGeneration/DlcDisableGeneration.cs
+++ X4SectorCreator/XmlGeneration/DlcDisableGeneration.cs
@@ -28,6 +28,9 @@ namespace X4SectorCreator.XmlGeneration
                 // Split Vendetta
                 CreateReplacementMdForDlc(folder, SectorMapForm.DlcMapping["Split Vendetta"], "setup_dlc_split", "story_split", "x4ep1_war_split");
 
+                // Hyperion Pack
+                CreateReplacementMdForDlc(folder, SectorMapForm.DlcMapping["Hyperion Pack"], "setup_dlc_mini_01", "story_hyperion", "gs_hyperion");
+
                 // Base Game
                 var baseGameMds = new List<string> { "story_buccaneers", "story_diplomacy_intro", "story_paranid", "story_research_welfare_1", "story_ventures", "terraforming", "x4ep1_war_subscriptions" };
 
