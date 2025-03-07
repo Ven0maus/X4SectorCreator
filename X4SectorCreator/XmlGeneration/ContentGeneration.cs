5c5b2fb5c7366e38a2eeb6da529e80421332a980
venomaus <stefvandenbruel@outlook.com>
2025-03-07 03:57:34 +0100

    fix bug with dependencies

--- X4SectorCreator/XmlGeneration/ContentGeneration.cs
+++ X4SectorCreator/XmlGeneration/ContentGeneration.cs
@@ -43,19 +43,27 @@ namespace X4SectorCreator.XmlGeneration
                         continue;
                     }
 
-                    // Each base game cluster, by default has no zones.
-                    // So if zones are present, it means a gate connection was added to a custom sector
-                    // This means this cluster is eligible for dependency check
-                    if (!string.IsNullOrWhiteSpace(cluster.Dlc))
+                    bool breaked = false;
+                    foreach (var zone in sector.Zones)
                     {
-                        if (dlcDependencies.Contains(cluster.Dlc))
+                        if (!zone.IsBaseGame)
                         {
-                            break;
-                        }
+                            if (!string.IsNullOrWhiteSpace(cluster.Dlc))
+                            {
+                                if (dlcDependencies.Contains(cluster.Dlc))
+                                {
+                                    breaked = true;
+                                    break;
+                                }
 
-                        _ = dlcDependencies.Add(cluster.Dlc);
-                        break;
+                                breaked = true;
+                                _ = dlcDependencies.Add(cluster.Dlc);
+                                break;
+                            }
+                        }
                     }
+                    if (breaked)
+                        break;
                 }
             }
 
