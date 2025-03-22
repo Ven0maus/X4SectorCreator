3be3f7521065cc5d80c830c331a3225575aa6da2
venomaus <stefvandenbruel@outlook.com>
2025-03-22 12:27:51 +0100

    removal

--- X4SectorCreator/XmlGeneration/Helpers/XmlNullableAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace X4SectorCreator.XmlGeneration.Helpers
-{
-    [AttributeUsage(AttributeTargets.Property)]
-    public class XmlNullableAttribute : Attribute
-    {
-        // This attribute is used for marking nullable properties
-    }
-}
